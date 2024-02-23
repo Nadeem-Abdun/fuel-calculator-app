fff43807cecc5e68c3a76cdff08accac65474d12
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-02-23 15:47:11 +0530

    Project Structure Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import React, { useState } from 'react';
-import { Box } from '@mui/material';
-import FuelCalculator from './Components/FuelCalculator';
-// import LoginPage from './Components/LoginPage';
+import { Grid } from '@mui/material';
+import FuelCalculator from './screens/FuelCalculator';
 
 function App() {
-  // Login Validation State
-  // const [isLoginValid, setisLoginValid] = useState(false);
   return (
-    <>
-      <Box height='100vh' width='100vw' display='flex' justifyContent='center' alignItems='center' sx={{ background: 'linear-gradient(to right, #733872, #ca4c7c)' }}>
-        {/* {!isLoginValid ?
-          <LoginPage isLoginValid={isLoginValid} setisLoginValid={setisLoginValid} />
-          :
-          <FuelCalculator isLoginValid={isLoginValid} setisLoginValid={setisLoginValid} />
-        } */}
+    <Grid container xs={12} justifyContent="center" alignItems="center" sx={{ background: "linear-gradient(to right, #733872, #ca4c7c)", height: "100dvh" }}>
+      <Grid item xl={4} lg={4} md={6} sm={8} xs={11}>
         <FuelCalculator />
-      </Box>
-    </>
-  );
+      </Grid>
+    </Grid>
+  )
 }
 
 export default App;
