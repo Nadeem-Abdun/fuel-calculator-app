263032ba075a4c7df10153b0d5c41cf1ff10fc38
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-02-24 22:27:49 +0530

    Removed un-necessary grids

--- src/screens/FuelCalculator.tsx
+++ src/screens/FuelCalculator.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
-import { Grid, Card, Typography, TextField, Button, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material'
-import LocalGasStationIcon from '@mui/icons-material/LocalGasStation';
-import SaveIcon from '@mui/icons-material/Save';
+import React, { useState } from "react";
+import { Grid, Card, Typography, TextField, Button, Dialog, DialogTitle, DialogContent, DialogActions } from "@mui/material";
+import LocalGasStationIcon from "@mui/icons-material/LocalGasStation";
+import SaveIcon from "@mui/icons-material/Save";
 
 function FuelCalculator() {
 
@@ -73,83 +73,81 @@ function FuelCalculator() {
     }
 
     return (
-        <Grid container xs={12} justifyContent='center' alignItems='center'>
-            <Grid item xs={12}>
-                <Card sx={{ paddingY: '25px' }}>
-                    <Grid container item xs={12} justifyContent='center' alignItems='center' spacing={2}>
+        <React.Fragment>
+            <Card sx={{ paddingY: "25px" }}>
+                <Grid container item xs={12} justifyContent="center" alignItems="center" spacing={2}>
+                    <Grid item xs={11}>
+                        <Typography variant="h4" textAlign="center" fontWeight="bold">Fuel Calculator</Typography>
+                    </Grid>
+                    <Grid item xs={11}>
+                        <Typography variant="subtitle2" color="#6D6C7D"><b>*</b> Required</Typography>
+                    </Grid>
+                    <Grid item xs={11}>
+                        <TextField
+                            id="distance"
+                            label="Distance in Kms"
+                            fullWidth
+                            type="number"
+                            value={fuelValues.distance}
+                            onChange={(e) => { handleFuelValues("distance", e.target.value); setemptyFieldsValidator(false) }}
+                            required
+                        />
+                    </Grid>
+                    <Grid item xs={11}>
+                        <TextField
+                            id="fuelPrice"
+                            label="Fuel Price in Rs"
+                            fullWidth
+                            type="number"
+                            value={fuelValues.fuelPrice}
+                            onChange={(e) => { handleFuelValues("fuelPrice", e.target.value); setemptyFieldsValidator(false) }}
+                            required
+                        />
+                    </Grid>
+                    <Grid item xs={11}>
+                        <TextField
+                            id="vehicleMileage"
+                            label="Vehicle Mileage in Km/L"
+                            fullWidth
+                            type="number"
+                            value={fuelValues.vehicleMileage}
+                            onChange={(e) => { handleFuelValues("vehicleMileage", e.target.value); setemptyFieldsValidator(false) }}
+                            required
+                        />
+                    </Grid>
+                    {emptyFieldsValidator &&
                         <Grid item xs={11}>
-                            <Typography variant='h4' textAlign='center' fontWeight='bold'>Fuel Calculator</Typography>
+                            <Typography variant="subtitle2" color="error" textAlign="center">"All fields are mandatory"</Typography>
                         </Grid>
-                        <Grid item xs={11}>
-                            <Typography variant='subtitle2' color='#6D6C7D'><b>*</b> Required</Typography>
+                    }
+                    <Grid item xs={11}>
+                        <Typography variant="body1">Fuel Quantity Estimated: <strong>{fuelValues.fuelQuantityEst}</strong> L </Typography>
+                        <Typography variant="body1">Fuel Cost Estimated: <strong>{fuelValues.fuelCostEst}</strong> Rs </Typography>
+                    </Grid>
+                    <Grid container item xs={11} justifyContent="center" alignItems="center" rowGap={1}>
+                        <Grid item xs={12}>
+                            <Button onClick={() => { handleCostQtyCalculations() }} startIcon={<LocalGasStationIcon />} color="success" variant="contained" fullWidth>Calculate</Button>
                         </Grid>
-                        <Grid item xs={11}>
-                            <TextField
-                                id='distance'
-                                label='Distance in Kms'
-                                fullWidth
-                                type='number'
-                                value={fuelValues.distance}
-                                onChange={(e) => { handleFuelValues("distance", e.target.value); setemptyFieldsValidator(false) }}
-                                required
-                            />
-                        </Grid>
-                        <Grid item xs={11}>
-                            <TextField
-                                id='fuelPrice'
-                                label='Fuel Price in Rs'
-                                fullWidth
-                                type='number'
-                                value={fuelValues.fuelPrice}
-                                onChange={(e) => { handleFuelValues("fuelPrice", e.target.value); setemptyFieldsValidator(false) }}
-                                required
-                            />
-                        </Grid>
-                        <Grid item xs={11}>
-                            <TextField
-                                id='vehicleMileage'
-                                label='Vehicle Mileage in Km/L'
-                                fullWidth
-                                type='number'
-                                value={fuelValues.vehicleMileage}
-                                onChange={(e) => { handleFuelValues("vehicleMileage", e.target.value); setemptyFieldsValidator(false) }}
-                                required
-                            />
-                        </Grid>
-                        {emptyFieldsValidator &&
-                            <Grid item xs={11}>
-                                <Typography variant='subtitle2' color='error' textAlign='center'>"All fields are mandatory"</Typography>
-                            </Grid>
-                        }
-                        <Grid item xs={11}>
-                            <Typography variant='body1'>Fuel Quantity Estimated: <strong>{fuelValues.fuelQuantityEst}</strong> L </Typography>
-                            <Typography variant='body1'>Fuel Cost Estimated: <strong>{fuelValues.fuelCostEst}</strong> Rs </Typography>
-                        </Grid>
-                        <Grid container item xs={11} justifyContent='center' alignItems='center' rowGap={1}>
-                            <Grid item xs={12}>
-                                <Button onClick={() => { handleCostQtyCalculations() }} startIcon={<LocalGasStationIcon />} color='success' variant='contained' fullWidth>Calculate</Button>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Button onClick={() => { handleRecordCalcDialog() }} startIcon={<SaveIcon />} color='success' variant='contained' fullWidth>Save Estimations</Button>
-                            </Grid>
+                        <Grid item xs={12}>
+                            <Button onClick={() => { handleRecordCalcDialog() }} startIcon={<SaveIcon />} color="success" variant="contained" fullWidth>Save Estimations</Button>
                         </Grid>
                     </Grid>
-                </Card>
-            </Grid>
+                </Grid>
+            </Card>
             {/* Record Calculations Dialog */}
             <Dialog open={recordCalcDialog} onClose={() => handleRecordCalcDialog()}>
                 <DialogTitle>
-                    <Typography variant='h6' fontWeight={500}>Record/Save Current Fuel Estimation</Typography>
+                    <Typography variant="h6" fontWeight={500}>Record/Save Current Fuel Estimation</Typography>
                 </DialogTitle>
                 <DialogContent>
                     <div style={{ marginTop: "16px" }}></div>
                     <Grid container xs={12} justifyContent="flex-start" alignItems="center" rowGap={2} columnSpacing={1}>
                         <Grid item xs={12}>
                             <TextField
-                                id='recordTitle'
-                                label='Title of the Record'
+                                id="recordTitle"
+                                label="Title of the Record"
                                 fullWidth
-                                type='text'
+                                type="text"
                                 value={fuelValues.recordTitle}
                                 onChange={(e) => handleFuelValues("recordTitle", e.target.value)}
                                 required
@@ -157,10 +155,10 @@ function FuelCalculator() {
                         </Grid>
                         <Grid item sm={6} xs={12}>
                             <TextField
-                                id='recordStartLocation'
-                                label='Start Location'
+                                id="recordStartLocation"
+                                label="Start Location"
                                 fullWidth
-                                type='text'
+                                type="text"
                                 value={fuelValues.recordStartLocation}
                                 onChange={(e) => handleFuelValues("recordStartLocation", e.target.value)}
                                 required
@@ -168,10 +166,10 @@ function FuelCalculator() {
                         </Grid>
                         <Grid item sm={6} xs={12}>
                             <TextField
-                                id='recordEndLocation'
-                                label='Destination'
+                                id="recordEndLocation"
+                                label="Destination"
                                 fullWidth
-                                type='text'
+                                type="text"
                                 value={fuelValues.recordEndLocation}
                                 onChange={(e) => handleFuelValues("recordEndLocation", e.target.value)}
                                 required
@@ -179,10 +177,10 @@ function FuelCalculator() {
                         </Grid>
                         <Grid item sm={4} xs={12}>
                             <TextField
-                                id='distance'
-                                label='Distance in Kms'
+                                id="distance"
+                                label="Distance in Kms"
                                 fullWidth
-                                type='number'
+                                type="number"
                                 value={fuelValues.distance}
                                 onChange={(e) => { handleFuelValues("distance", e.target.value) }}
                                 required
@@ -190,10 +188,10 @@ function FuelCalculator() {
                         </Grid>
                         <Grid item sm={4} xs={12}>
                             <TextField
-                                id='fuelPrice'
-                                label='Fuel Price in Rs'
+                                id="fuelPrice"
+                                label="Fuel Price in Rs"
                                 fullWidth
-                                type='number'
+                                type="number"
                                 value={fuelValues.fuelPrice}
                                 onChange={(e) => { handleFuelValues("fuelPrice", e.target.value) }}
                                 required
@@ -201,22 +199,22 @@ function FuelCalculator() {
                         </Grid>
                         <Grid item sm={4} xs={12}>
                             <TextField
-                                id='vehicleMileage'
-                                label='Vehicle Mileage in Km/L'
+                                id="vehicleMileage"
+                                label="Vehicle Mileage in Km/L"
                                 fullWidth
-                                type='number'
+                                type="number"
                                 value={fuelValues.vehicleMileage}
                                 onChange={(e) => { handleFuelValues("vehicleMileage", e.target.value) }}
                                 required
                             />
                         </Grid>
                         <Grid item sm={6} xs={12}>
-                            <Typography variant='body1'>Fuel Quantity Estimated:</Typography>
-                            <Typography variant='body1' fontWeight={600}>{fuelValues.fuelQuantityEst} L </Typography>
+                            <Typography variant="body1">Fuel Quantity Estimated:</Typography>
+                            <Typography variant="body1" fontWeight={600}>{fuelValues.fuelQuantityEst} L </Typography>
                         </Grid>
                         <Grid item sm={6} xs={12}>
-                            <Typography variant='body1'>Fuel Cost Estimated:</Typography>
-                            <Typography variant='body1' fontWeight={600}>{fuelValues.fuelCostEst} Rs </Typography>
+                            <Typography variant="body1">Fuel Cost Estimated:</Typography>
+                            <Typography variant="body1" fontWeight={600}>{fuelValues.fuelCostEst} Rs </Typography>
                         </Grid>
                     </Grid>
                 </DialogContent>
@@ -225,7 +223,7 @@ function FuelCalculator() {
                     <Button color="success" variant="contained" onClick={() => handleRecordsStorage()}>Save</Button>
                 </DialogActions>
             </Dialog>
-        </Grid>
+        </React.Fragment>
     )
 }
 
