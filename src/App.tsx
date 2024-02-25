2c653c53e4c450529d868ed61fe47d538bc126b4
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-02-25 19:11:20 +0530

    React Router Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import React, { useState } from 'react';
-import { Grid } from '@mui/material';
-import FuelCalculator from './screens/FuelCalculator';
+import React from "react";
+import { Grid } from "@mui/material";
+import FuelCalculator from "./screens/FuelCalculator";
+import SavedEstimations from "./screens/SavedEstimations";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import packageJson from "../package.json";
+
 
 function App() {
+
+  const basename = packageJson.homepage ? new URL(packageJson.homepage).pathname : '/';
+
   return (
-    <Grid container xs={12} justifyContent="center" alignItems="center" sx={{ background: "linear-gradient(to right, #733872, #ca4c7c)", height: "100dvh" }}>
-      <Grid item xl={4} lg={4} md={6} sm={8} xs={11}>
-        <FuelCalculator />
+    <BrowserRouter basename={basename}>
+      <Grid container xs={12} justifyContent="center" alignItems="center" sx={{ background: "linear-gradient(to right, #733872, #ca4c7c)", height: "100dvh" }}>
+        <Grid item xl={4} lg={4} md={6} sm={8} xs={11}>
+          <Routes>
+            <Route path="/" element={<FuelCalculator />} />
+            <Route path="/saved-estimations" element={<SavedEstimations />} />
+          </Routes>
+        </Grid>
       </Grid>
-    </Grid>
+    </BrowserRouter>
   )
 }
 
-export default App;
+export default App
