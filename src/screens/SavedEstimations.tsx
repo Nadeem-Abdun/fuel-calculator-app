78c0df83b97c3a1437869ed9b47362c5fa444d7f
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2024-02-28 23:00:59 +0530

    List Mapping Delete Functionality Added

--- src/screens/SavedEstimations.tsx
+++ src/screens/SavedEstimations.tsx
@@ -1,8 +1,32 @@
-import React from "react";
-import { Grid, Card, Typography, IconButton } from "@mui/material";
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import React, { useState, useEffect } from "react";
+import { Grid, Card, Typography, IconButton, Button } from "@mui/material";
+import { ExitToApp, DeleteForever } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
 
 function SavedEstimations() {
+
+    const history = useNavigate();
+
+    const [fuelRecords, setFuelRecords] = useState([]);
+
+    const handleDeleteRecord = (index: any) => {
+        const updatedRecords = [...fuelRecords];
+        updatedRecords.splice(index, 1); // Remove the record at the given index
+        setFuelRecords(updatedRecords); // Update the state
+        localStorage.setItem("fuelRecords", JSON.stringify(updatedRecords)); // Update local storage
+    };
+
+    const navigateToFuelCalculator = () => {
+        history("/")
+    }
+
+    useEffect(() => {
+        const savedFuelRecords = localStorage.getItem("fuelRecords");
+        if (savedFuelRecords) {
+            setFuelRecords(JSON.parse(savedFuelRecords));
+        }
+    }, [])
+
     return (
         <Card sx={{ paddingY: "25px", minHeight: "457px" }}>
             <Grid container item xs={12} justifyContent="center" alignItems="center" rowGap={2}>
@@ -10,49 +34,67 @@ function SavedEstimations() {
                     <Typography variant="h4" textAlign="center" fontWeight="bold">Saved Estimations</Typography>
                 </Grid>
                 <Grid container item xs={12} justifyContent="center" alignItems="flex-start" sx={{ maxHeight: "400px", overflowY: "auto" }}>
-                    <Grid item xs={12}>
-                        <Card sx={{ marginX: "5px", paddingX: "5px", paddingY: "3px", minHeight: "85px", background: "linear-gradient(to right, #d2d2d2, #d4d4d4)" }}>
-                            <Grid container xs={12} justifyContent="center" alignItems="center" rowGap={1}>
-
-                                <Grid container item xs={12} justifyContent="space-between" alignItems="center">
-                                    <Grid item>
-                                        <Typography variant="h6" fontWeight={600}>Estimation Title</Typography>
-                                    </Grid>
-                                    <Grid item>
-                                        <IconButton size="small"><DeleteForeverIcon color="error" /></IconButton>
-                                    </Grid>
+                    {fuelRecords.length !== 0 ?
+                        fuelRecords && fuelRecords.map((record, index) => {
+                            const { distance, fuelPrice, vehicleMileage, recordTitle, recordStartLocation, recordEndLocation, fuelQuantityEst, fuelCostEst, createdAt } = record;
+                            return (
+                                <Grid item xs={12} key={index}>
+                                    <Card sx={{ marginX: "5px", paddingX: "5px", paddingY: "3px", minHeight: "85px", background: "linear-gradient(to right, #d2d2d2, #d4d4d4)" }}>
+                                        <Grid container xs={12} justifyContent="center" alignItems="center">
+                                            <Grid container item xs={12} justifyContent="space-between" alignItems="center">
+                                                <Grid item>
+                                                    <Typography variant="h6" fontWeight={600}>{recordTitle}</Typography>
+                                                </Grid>
+                                                <Grid item>
+                                                    <IconButton size="small" onClick={() => handleDeleteRecord(index)}><DeleteForever color="error" fontSize="small" /></IconButton>
+                                                </Grid>
+                                            </Grid>
+                                            <Grid container item xs={12}>
+                                                <Grid container item xs={10}>
+                                                    <Grid item xs={12}>
+                                                        <Typography variant="body2" fontWeight={400}>Location: <strong>{recordStartLocation}</strong>  - <strong>{recordEndLocation}</strong></Typography>
+                                                    </Grid>
+                                                    <Grid item xs={12}>
+                                                        <Typography variant="body2" fontWeight={400}>Given Distance: <strong>{distance}kms</strong></Typography>
+                                                    </Grid>
+                                                    <Grid item xs={12}>
+                                                        <Typography variant="body2" fontWeight={400}>Given Fuel Price: <strong>{fuelPrice}rs</strong></Typography>
+                                                    </Grid>
+                                                    <Grid item xs={12}>
+                                                        <Typography variant="body2" fontWeight={400}>Given Mileage: <strong>{vehicleMileage}km/l</strong></Typography>
+                                                    </Grid>
+                                                    <Grid item xs={12}>
+                                                        <Typography variant="caption" fontWeight={400}>Saved @ {createdAt}</Typography>
+                                                    </Grid>
+                                                </Grid>
+                                                <Grid container item xs={2}>
+                                                    <Grid item xs={12}>
+                                                        <Typography variant="body1" fontWeight={600}>{fuelQuantityEst}L</Typography>
+                                                    </Grid>
+                                                    <Grid item xs={12}>
+                                                        <Typography variant="body2" fontWeight={400}>Qty. Req.</Typography>
+                                                    </Grid>
+                                                    <Grid item xs={12}>
+                                                        <Typography variant="body1" fontWeight={600}>{fuelCostEst}Rs</Typography>
+                                                    </Grid>
+                                                    <Grid item xs={12}>
+                                                        <Typography variant="body2" fontWeight={400}>Cost. Est.</Typography>
+                                                    </Grid>
+                                                </Grid>
+                                            </Grid>
+                                        </Grid>
+                                    </Card>
                                 </Grid>
-
-                                <Grid container item xs={12}>
-                                    <Grid container item xs={10}>
-                                        <Grid item xs={12}>
-                                            <Typography variant="body1">From Location</Typography>
-                                            <Typography variant="body1">To</Typography>
-                                            <Typography variant="body1">To Location</Typography>
-                                        </Grid>
-                                        <Grid item xs={12}>
-                                            <Typography variant="subtitle1">Created @ {Date.now().toPrecision()}</Typography>
-                                        </Grid>
-                                    </Grid>
-                                    <Grid container item xs={2}>
-                                        <Grid item xs={12}>
-                                            <Typography variant="h6" fontWeight={600}>0.7 L</Typography>
-                                        </Grid>
-                                        <Grid item xs={12}>
-                                            <Typography variant="body2" fontWeight={400}>Qty. Req.</Typography>
-                                        </Grid>
-                                        <Grid item xs={12}>
-                                            <Typography variant="h6" fontWeight={600}>30 Rs</Typography>
-                                        </Grid>
-                                        <Grid item xs={12}>
-                                            <Typography variant="body2" fontWeight={400}>Cost. Est.</Typography>
-                                        </Grid>
-                                    </Grid>
-                                </Grid>
-
-                            </Grid>
-                        </Card>
-                    </Grid>
+                            )
+                        })
+                        :
+                        <Grid item xs={12}>
+                            <Typography variant="h6" textAlign="center">No Records Found</Typography>
+                        </Grid>
+                    }
+                </Grid>
+                <Grid item xs={11}>
+                    <Button onClick={() => { navigateToFuelCalculator() }} startIcon={<ExitToApp />} color="success" variant="contained" fullWidth>Go To Fuel Calculator</Button>
                 </Grid>
             </Grid>
         </Card >
